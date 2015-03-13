640344b286aaf972e21bd1f2cff7a176f36c33c9
Yeongdo Jin <uspoint@naver.com>
2015-03-13 16:50:41 +0900

    Start

---
+++ cal_spring/src/main/java/play/jin/cal/test/TestController.java
@@ -0,0 +1,10 @@
+package play.jin.cal.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+	private static Logger logger = LoggerFactory.getLogger(TestController.class);
+}
